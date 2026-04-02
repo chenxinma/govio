d7cfeb35c618ba17ccb094290e622f8712b07973
chenxin.ma <chenxin.ma@qq.com>
2026-04-02 12:18:04 +0800

    feat: register onboard CLI entry point

--- src/govio/__init__.py
+++ src/govio/__init__.py
@@ -3,5 +3,13 @@ from .metadata.gen_networkx import gml_generate
 from .graph.falkordb_graph import FalkorDBGraph
 from .graph.networkx_graph import NetworkXGraph
 from .mcp.server import create_server
+from .cli import onboard
 
-__all__ = ["run", "gml_generate", "FalkorDBGraph", "NetworkXGraph", "create_server"]
+__all__ = [
+    "run",
+    "gml_generate",
+    "FalkorDBGraph",
+    "NetworkXGraph",
+    "create_server",
+    "onboard",
+]
